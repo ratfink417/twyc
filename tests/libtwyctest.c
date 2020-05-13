976c8e156d090b6c6c6bdc58ab5b408259d1942a
ratfink417 <ratfink417@gmail.com>
2020-05-12 21:53:30 -0700

    updated unittests, still mostly broken

--- tests/libtwyctest.c
+++ tests/libtwyctest.c
@@ -3,71 +3,68 @@
 #include <dlfcn.h>
 
 #define import_check(TYPE,FUNC) TYPE TYPE ## _test_func = dlsym(lib_handle, #FUNC );\
-				check(TYPE ## _test_func != NULL, "failed import: " #FUNC );\
-				printf("loaded: %s\n", " " #FUNC);
+    check(TYPE ## _test_func != NULL, "failed import: " #FUNC );\
+    printf("loaded: %s\n", " " #FUNC);
 
-#define make_network_test(NUM, INPUT) Network *test_net ## NUM = make_network_test_func(INPUT);\
-					printf("Network: " #test_net ## NUM "\n");\
-					printf("Type: " #INPUT "\n");\
-					free(test_net ## NUM)
+#define make_network_test(NUM, INPUT) make_network test_net ## NUM = make_network_test_func(INPUT);\
+    free(test_net ## NUM);
 
 void *lib_handle = NULL;
+typedef char* (*spanner_tapper)(void);
+typedef Network* (*make_network)(char*);
+typedef void (*verifier)(char*, void*);
+typedef char* (*what_to_do)(Network*);
 
 char *code_relocation_check(){
-	typedef char* (*spanner_tapper)(void);
-	typedef Network* (*make_network)(char*);
-	typedef void (*verifier)(char*, void*);
-	typedef char* (*what_to_do)(Network*);
-
-	char *twyc_lib = "libtwyc.so";
-	lib_handle = dlopen(twyc_lib, RTLD_NOW);
-	check(lib_handle != NULL, "Failed to open library %s: %s", twyc_lib, dlerror());
+    char *twyc_lib = "libtwyc.so";
+    lib_handle = dlopen(twyc_lib, RTLD_NOW);
+    check(lib_handle != NULL, "Failed to open library %s: %s", twyc_lib, dlerror());
 
 // place any new function from twyc lib that will be used in your program here
-	import_check(spanner_tapper, tap);
-    	import_check(make_network, new_net);
-    	import_check(verifier, span_or_tap_only);
-    	import_check(what_to_do, wut_do);
+    import_check(spanner_tapper, tap);
+    import_check(make_network, new_net);
+    import_check(verifier, span_or_tap_only);
+    import_check(what_to_do, wut_do);
 
-	return NULL;
+    return NULL;
 
 error:
-	return "Failed the code relocation test check the spelling or type signature of the\n\
+    return "Failed the code relocation test check the spelling or type signature of the\n\
 function or library you are trying to import\n";
 }
 
 char *test_new_net(){
-	make_network_test(1,"can");
-	make_network_test(2,"must");
-	make_network_test(3,"atypical");
+    make_network_test(1,can);
+    make_network_test(2,must);
+    make_network_test(3,atypical);
 
-	return NULL;
+    return NULL;
 }
 char *test_span_or_tap_only(){
-	return NULL;
+    return NULL;
 }
 char *test_span_and_tap(){
-	return NULL;
+    return NULL;
 }
 char *test_wut_do(){
-	return NULL;
+    return NULL;
 }
 
 char *alltests(){
         unittest_suite_start();
         unittest_run_test(code_relocation_check);
-  	unittest_run_test(test_new_net);
+        unittest_run_test(test_new_net);
         unittest_run_test(test_span_or_tap_only);
         unittest_run_test(test_span_and_tap);
         unittest_run_test(test_wut_do);
 
-	int rc = dlclose(lib_handle);
-	check(rc == 0, "Failed to close shared library");
+        int rc = dlclose(lib_handle);
+        check(rc == 0, "Failed to close shared library");
 
-	return NULL;
+        return NULL;
 
 error:
-	return "ERROR";
+        return "ERROR";
 }
 
 RUN_TESTS(alltests);
